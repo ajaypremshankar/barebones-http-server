4283aaef54469b4422bcd4bf30f94644fcdccfba
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-10-29 22:27:54 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import socket  # noqa: F401
 
+def create_http_response(status, headers = "", body = ""):
+    return f"HTTP/1.1 {status}\r\n{headers}\r\n{body}"
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()[0].sendall(create_http_response("200 OK").encode())
 
 
 if __name__ == "__main__":
