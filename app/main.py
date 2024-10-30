86bf0f7b7b342409478ec27813bd751356ed3e08
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-10-30 21:42:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-import socket  # noqa: F401
+import socket
+import sys
 import threading
-from threading import Thread
+
+base_path = None
 
 
-def create_http_response(status, headers="", body=""):
-    return f"HTTP/1.1 {status}\r\n{headers}\r\n\r\n{body}"
+def create_http_response(status, headers=None, body=""):
+    headers_str = ""
+    if headers is not None:
+        for k, v in headers.items():
+            headers_str += f"{k.strip()}:{str(v).strip()}\r\n"
+
+    return f"HTTP/1.1 {status}\r\n{headers_str}\r\n{body}"
 
 
 def parse_buffer(buff):
@@ -26,6 +33,12 @@ def parse_buffer(buff):
         "body": split_buff[1]
     }
 
+
+def send_all(sock: socket.socket, status, headers = None, body = ""):
+    sock.sendall(create_http_response(status, headers, body).encode())
+    sock.close()
+
+
 def handle_request(sock: socket.socket):
     buff = sock.recv(1024)
 
@@ -39,25 +52,53 @@ def handle_request(sock: socket.socket):
     elif method == 'GET' and path.startswith("/echo/"):
         path_split = path.rsplit("/", 1)
         echo_val = path_split[1]
-        resp_headers = f"Content-Type: text/plain\r\nContent-Length: {len(echo_val)}"
 
-        sock.sendall(create_http_response("200 OK", resp_headers, echo_val).encode())
-        sock.close()
+        send_all(sock, "200 OK", {
+            "Content-Type": "text/plain",
+            "Content-length": len(echo_val)
+        }, echo_val)
     elif method == 'GET' and path == '/user-agent':
         user_agent = parsed_request.get("headers").get("User-Agent")
-        resp_headers = f"Content-Type: text/plain\r\nContent-Length: {len(user_agent)}"
-        sock.sendall(create_http_response("200 OK", resp_headers, user_agent).encode())
-        sock.close()
+
+        send_all(sock, "200 OK", {
+            "Content-Type": "text/plain",
+            "Content-length": len(user_agent)
+        }, user_agent)
+
+    elif method == 'GET' and path.startswith('/files'):
+
+        if base_path is None:
+            sock.sendall(create_http_response("404 Not Found").encode())
+            sock.close()
+        path_split = path.rsplit("/", 1)
+        file_path = path_split[1]
+        content = ""
+        try:
+            f = open(f"{base_path}{file_path}", "r")
+            content = f.read()
+            send_all(sock, "200 OK", {
+                "Content-Type": "application/octet-stream",
+                "Content-length": len(content)
+            }, content)
+        except FileNotFoundError:
+            send_all(sock, "404 Not Found")
+
     else:
-        sock.sendall(create_http_response("404 Not Found").encode())
+        send_all(sock, "404 Not Found")
 
 def main():
+    global base_path
+
+    if len(sys.argv) >= 2:
+        base_path = sys.argv[2]
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
         sock, response_addr = server_socket.accept()
         t = threading.Thread(target=lambda: handle_request(sock))
         t.start()
+
 
 if __name__ == "__main__":
     main()
